384134d1abd30dbe69ac4b90fee642503fa2d63f
Dominic Lowes <domin@Dominics-MacBook-Air.local>
2022-03-30 10:12:44 +0900

    completed sumTilZero

---
+++ sumTilZero.js
@@ -0,0 +1,10 @@
+const tillZero = (nums) => {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++){
+      if (nums[i] === 0) {
+        break;
+    } else {
+      result += nums[i]
+    }}
+      return result;
+  };
